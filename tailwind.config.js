85e09ee602b440c6ab67098b5addc83401b85d53
c-lamd <c.lamd24@gmail.com>
2025-03-09 13:41:39 -0700

    Adding fonts and trying to set up theme

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Primary colors
+        light: '#fff5dd',
+        dark: '#1F1818',
+        accent: '#A3865F',
+        brown: {
+          DEFAULT: '#A3865F',
+          dark: '#4E3E2F',
+        },
+      }
+    }
+  }
+} 
