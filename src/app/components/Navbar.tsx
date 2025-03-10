68c7df38f5517d0bc6729c934ccba24c1967392d
c-lamd <c.lamd24@gmail.com>
2025-03-09 22:12:20 -0700

    Navbar edits importing Lucide

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,28 +36,12 @@ const Navbar = () => {
             onClick={toggleMenu}
             className="text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
+
               {isMenuOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
+                <X className="h-6 w-6" />
               ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
+                <Menu className="h-6 w-6" />
               )}
-            </svg>
           </button>
         </div>
       </div>
