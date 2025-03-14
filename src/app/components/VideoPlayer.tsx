444d9078a899b7a9704c50c125794d1167b9f83b
c-lamd <c.lamd24@gmail.com>
2025-03-13 23:37:37 -0700

    Fixing video not playing on iphone

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -1,18 +1,27 @@
 'use client'
+import { useRef, useEffect } from 'react';
 
 export default function VideoPlayer() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    
+    useEffect(() => {
+        // Ensure video plays when component mounts
+        if (videoRef.current) {
+            videoRef.current.play().catch(err => {
+                console.error("Error playing video:", err);
+            });
+        }
+    }, []);
+
     return (
-        <div id="video-player" className="flex-1 flex flex-col text-left h-full">
+        <div className="flex-1 flex flex-col text-left h-full">
             <video
+                ref={videoRef}
                 autoPlay
                 muted
                 loop
                 playsInline
                 className="w-full h-full object-cover"
-                onLoadedData={() => {
-                    const video = document.getElementById('video-player') as HTMLVideoElement;
-                    video.play();
-                }}
             >
                 <source src="/videos/b-roll-footage-white-intro.mp4" type="video/mp4" />
             </video>
