2681f60b399470936dc969963223709f428e53c0
c-lamd <c.lamd24@gmail.com>
2025-03-19 18:47:25 -0700

    Adding arialabels to videoplayer

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -3,7 +3,7 @@ import { useRef, useEffect } from 'react';
 
 export default function VideoPlayer() {
     const videoRef = useRef<HTMLVideoElement>(null);
-    
+
     useEffect(() => {
         // Ensure video plays when component mounts
         if (videoRef.current) {
@@ -14,18 +14,25 @@ export default function VideoPlayer() {
     }, []);
 
     return (
-        <div className="flex-1 flex flex-col text-left h-full animate-fadeIn-2000">
+        <div className="flex-1 flex flex-col text-left h-full animate-fadeIn-1000" aria-label='Featured Barista video'>
             <video
                 ref={videoRef}
+                className="w-full h-full object-cover"
                 autoPlay
                 muted
                 loop
                 playsInline
-                className="w-full h-full object-cover"
+                aria-label='B-roll showcase video featuring latte art and baristas working'
             >
                 <source src="/videos/b-roll-footage.mp4" type="video/mp4" />
                 {/* <source src="/videos/b-roll-footage-white-intro.mp4" type="video/mp4" /> */}
+                Your browser does not support the video tag.
             </video>
+            
+            {/* Text description of video content */}
+            <div className="sr-only">
+                This video showcases a Porsche vehicle, highlighting its exterior design, performance features, and dynamic movement on the road.
+            </div>
         </div>
     );
 };
